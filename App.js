8f00d48cc5e0aae0631d5ddcd55cf8c25371bf91
Joanne0119 <liuchengen52033@gmail.com>
2024-03-14 10:54:53 +0800

    add Header

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './src/screens/HomeScreen';
 import DetailScreen from './src/screens/DetailScreen';
 import CardPostScreen from './src/screens/CardPostScreen';
+import Header from './src/components/Header';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer initialRouteName="Home">
+      <StatusBar />
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailScreen} />
-        <Stack.Screen name="CardPost" component={CardPostScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} 
+          options={{ 
+            headerTitle: (props) => <Header {...props} /> ,
+            headerLeft: () => 
+              <Image 
+                source={{uri: 'https://github.com/Joanne0119/book_app/blob/master/assets/image/icon/nav_icon.png?raw=true'}}
+                style={{width: 24, height: 24, margin: 20}}
+              />,
+            headerRight: () => 
+              <Image 
+                source={{uri: 'https://github.com/Joanne0119/book_app/blob/master/assets/image/icon/search_icon.png?raw=true'}}
+                style={{width: 24, height: 24, margin: 20}}
+              />
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailScreen} 
+          options={{ 
+            headerTitle: (props) => <Header {...props} /> ,
+            headerRight: () => 
+              <Image 
+                source={{uri: 'https://github.com/Joanne0119/book_app/blob/master/assets/image/icon/search_icon.png?raw=true'}}
+                style={{width: 24, height: 24, margin: 20}}
+              />
+          }}
+        />
+        <Stack.Screen name="CardPost" component={CardPostScreen} 
+          options={{ 
+            headerTitle: (props) => <Header {...props} /> ,
+            headerRight: () => 
+              <Image 
+                source={{uri: 'https://github.com/Joanne0119/book_app/blob/master/assets/image/icon/search_icon.png?raw=true'}}
+                style={{width: 24, height: 24, margin: 20}}
+              />
+        }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
