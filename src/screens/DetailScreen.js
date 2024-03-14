318e65183e4b2473a898f2948a8063c331b96802
Joanne0119 <liuchengen52033@gmail.com>
2024-03-14 10:15:34 +0800

    make home link to detail page

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 
-const DetailScreen = () => {
+const DetailScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Pressable
+        onPress={() => navigation.push('Details')}
+      >
+        <Text style={{ color: 'blue' }}>
+          Go to Details ...again
+        </Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate('Home')}
+      >
+        <Text style={{ color: 'blue' }}>
+          Go Home
+        </Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.goBack()}
+      >
+        <Text style={{ color: 'blue' }}>
+          Go Back
+        </Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.popToTop()}
+      >
+        <Text style={{ color: 'blue' }}>
+          Go Back to the First Screen
+        </Text>
+      </Pressable>
   </View>
   )
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Pressable
+        onPress={() => navigation.navigate('Details')}
+      >
+        <Text style={{ color: 'blue' }}>
+          Go to Details
+        </Text>
+      </Pressable>
     </View>
   )
 }
