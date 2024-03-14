69bdede7a26702107c5d1bd6236f878281f22c60
Joanne0119 <liuchengen52033@gmail.com>
2024-03-14 10:27:13 +0800

    add params to detail page

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,12 +1,17 @@
 import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 
-const DetailScreen = ({ navigation }) => {
+const DetailScreen = ({ route, navigation }) => {
+  // const { itemId, itemTitle } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(route.params.itemId)}</Text>
+      <Text>itemTitle: {JSON.stringify(route.params.itemTitle)}</Text>
       <Pressable
-        onPress={() => navigation.push('Details')}
+        onPress={() => navigation.push('Details', {
+          itemId: Math.floor(Math.random() * 100),
+        })}
       >
         <Text style={{ color: 'blue' }}>
           Go to Details ...again
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,11 +2,15 @@ import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const HomeScreen = ({ navigation }) => {
+  const route = {
+    itemId: '86',
+    itemTitle: 'test'
+  }
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Pressable
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.navigate('Details', route)}
       >
         <Text style={{ color: 'blue' }}>
           Go to Details
